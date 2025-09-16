02edf3ae9bd0615f4f7f1da91c30a02db595dcb8
unknown <titouan.nicoullaud@gmail.com>
2025-09-16 15:37:22 +0200

    mise en place classe forme

--- formes_geometriques_Titouan/Forme.cs
+++ formes_geometriques_Titouan/Forme.cs
@@ -9,12 +9,46 @@ namespace formes_geometriques_Titouan
     class Forme
     {
         #region Attributs
-        #endregion
-        #region Propriétés
+        private Point point;
         #endregion
         #region Constructeurs
+        /// <summary>
+        /// Crée une forme avec un point à l'origine (0,0).
+        /// </summary>
+        public Forme()
+        {
+            point = new Point();
+        }
+        /// <summary>
+        /// Crée une forme avec un point aux coordonnées (x,y).
+        /// </summary>
+        public Forme(int x, int y)
+        {
+            point = new Point(x, y);
+        }
+        #endregion
+        #region Propriétés
+        public Point Point
+        {
+            get { return point; }
+            set { point = value; }
+        }
         #endregion
         #region Méthodes
+        /// <summary>
+        /// Renvoie 0 car une forme n'a pas d'aire définie.
+        /// </summary>
+        public float Aire()
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Renvoie 0 car une forme n'a pas de périmètre défini.
+        /// </summary>
+        public float Perimetre()
+        {
+            return 0;
+        }
         #endregion
     }
 }
