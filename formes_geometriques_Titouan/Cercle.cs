ea5e34bb32fd7e7b8d6de826dd80e9925399b942
unknown <titouan.nicoullaud@gmail.com>
2025-09-16 12:29:00 +0200

    création des différentes classe

---
+++ formes_geometriques_Titouan/Cercle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formes_geometriques_Titouan
+{
+    class Cercle
+    {
+        #region Attributs
+        #endregion
+        #region Propriétés
+        #endregion
+        #region Constructeurs
+        #endregion
+        #region Méthodes
+        #endregion
+    }
+}
---
+++ formes_geometriques_Titouan/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formes_geometriques_Titouan
+{
+    class Rectangle
+    {
+        #region Attributs
+        #endregion
+        #region Propriétés
+        #endregion
+        #region Constructeurs
+        #endregion
+        #region Méthodes
+        #endregion
+
+    }
+}
